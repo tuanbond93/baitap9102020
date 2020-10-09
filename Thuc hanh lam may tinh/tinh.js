3e606f384d00b5ec603a1a8d04c55644416bcd06
Tuan <sontuandav@gmail.com>
2020-10-09 21:50:37 +0700

    thunghiem

--- Thuc hanh lam may tinh/tinh.js
+++ Thuc hanh lam may tinh/tinh.js
@@ -1,32 +1,44 @@
 function tich() {
-       let soThuNhat = Number(document.getElementById("soThuNhat").value)
+    let soThuNhat = Number(document.getElementById("soThuNhat").value)
     let soThuHai = Number(document.getElementById("soThuHai").value)
     let tich = soThuNhat * soThuHai
     let phatNgon = tich
+    document.getElementById("pheptinh").innerText = soThuNhat + " * " + soThuHai + " = "
     document.getElementById("phatNgon").innerText = phatNgon
+    document.getElementById("pheptinhtam").value = phatNgon
 
 }
 function thuong() {
-    
+
     let soThuNhat = Number(document.getElementById("soThuNhat").value)
     let soThuHai = Number(document.getElementById("soThuHai").value)
     let thuong = soThuNhat / soThuHai
     let phatNgon = thuong
+    
+    document.getElementById("pheptinh").innerText = soThuNhat + " / " + soThuHai + " = "
     document.getElementById("phatNgon").innerText = phatNgon
+    document.getElementById("pheptinhtam").value = phatNgon
 }
 function tong() {
-        
-        let soThuNhat = Number(document.getElementById("soThuNhat").value)
-        let soThuHai = Number(document.getElementById("soThuHai").value)
-        let tong = soThuNhat + soThuHai
-        let phatNgon = tong
-        document.getElementById("phatNgon").innerText = phatNgon
+
+    let soThuNhat = Number(document.getElementById("soThuNhat").value)
+    let soThuHai = Number(document.getElementById("soThuHai").value)
+    let tong = soThuNhat + soThuHai
+    let phatNgon = tong
+    
+    document.getElementById("pheptinh").innerText = soThuNhat + " + " + soThuHai + " = "
+    document.getElementById("phatNgon").innerText = phatNgon
+    document.getElementById("pheptinhtam").value = phatNgon
 }
 function hieu() {
-            
-            let soThuNhat = Number(document.getElementById("soThuNhat").value)
-            let soThuHai = Number(document.getElementById("soThuHai").value)
-            let hieu = soThuNhat - soThuHai
-            let phatNgon = hieu
-            document.getElementById("phatNgon").innerText = phatNgon
+
+    let soThuNhat = Number(document.getElementById("soThuNhat").value)
+    let soThuHai = Number(document.getElementById("soThuHai").value)
+    let hieu = soThuNhat - soThuHai
+    let phatNgon = hieu
+    
+    document.getElementById("pheptinh").innerText = soThuNhat + " - " + soThuHai + " = "
+    document.getElementById("phatNgon").innerText = phatNgon
+    document.getElementById("pheptinhtam").value = phatNgon
 }
+
